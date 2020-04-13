0005db76c1a1b126f183420c6b0b409608e3b8d4
hppRC <hpp.ricecake@gmail.com>
2020-04-14 00:22:19 +0900

    chore

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-if [ ! which cargo ]; then
+if [ ! $(which cargo) ]; then
     # install Rust and tool chains
     # this is official installation of Rust
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -10,7 +10,7 @@ if [ ! -e /usr/local/opt/binutils/bin/strip ]; then
     brew install binutils
 fi
 
-if [ ! which cargo-atcoder ]; then
+if [ ! $(which cargo-atcoder) ]; then
     # install for cargo-atcoder
     cargo install --git https://github.com/tanakh/cargo-atcoder.git
 fi
