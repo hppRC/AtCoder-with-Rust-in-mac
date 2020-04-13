52b81aba277ffefbcbc14c9202a8dedad210b42b
hppRC <hpp.ricecake@gmail.com>
2020-04-14 00:00:30 +0900

    chore

--- init.sh
+++ init.sh
@@ -9,3 +9,10 @@ ln -sf $(pwd)/cargo-atcoder.toml ~/Library/Preferences/
 if [ ! -e /usr/local/opt/binutils/bin/strip ]; then
     brew install binutils
 fi
+
+if [! which cargo-atcoder]; then
+    # install for cargo-atcoder
+    cargo install --git https://github.com/tanakh/cargo-atcoder.git
+fi
+
+cargo atcoder login
