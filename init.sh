08fe64e4ca611bc85f5b46fd5da128dfd64d989f
hppRC <hpp.ricecake@gmail.com>
2020-04-13 23:54:43 +0900

    chore

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-if [ ! $(which cargo) ]; then
+if [ ! which cargo ]; then
     # install Rust and tool chains
     # this is official installation of Rust
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
